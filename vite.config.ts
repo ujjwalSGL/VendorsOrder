141c44d41a353c944be31b44afc7e1cf2f2a620d
ujjwal105 <ujjwal.tyagi@shipglobal.in>
2025-02-01 11:13:06 +0530

    shadcn setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
